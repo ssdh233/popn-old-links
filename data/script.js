7871409d4ba60a951b218ee86134eaf25668a719
ssdh233 <songsongdahu233@gmail.com>
2021-12-18 04:46:13 +0900

    Update data for 20 & 21

--- data/script.js
+++ data/script.js
@@ -1,3 +1,4 @@
+// for 22~24
 (function () {
   const arr = Array.from(
     document.querySelectorAll("ul.musiclist > li > div")
@@ -14,5 +15,44 @@
       };
     }
   });
+  console.log(JSON.stringify(arr));
+})();
+
+// for 21
+(function () {
+  const arr = Array.from(document.querySelectorAll("img[width='205']")).map(
+    (x) => {
+      if (x.parentNode.nodeName === "A") {
+        return {
+          detail: x.parentNode.href,
+          banner: x.src,
+        };
+      } else {
+        return {
+          banner: x.src,
+        };
+      }
+    }
+  );
+  console.log(JSON.stringify(arr));
+})();
+
+// for 20
+(function () {
+  const arr = Array.from(document.querySelectorAll("img[width='183']")).map(
+    (x) => {
+      if (x.parentNode.nodeName === "A") {
+        return {
+          detail: x.parentNode.href,
+          banner: x.src,
+        };
+      } else {
+        return {
+          banner: x.src,
+        };
+      }
+    }
+  );
+  console.log(arr.length);
   console.log(JSON.stringify(arr));
 })();
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,43 @@
 import { promises as fs } from "fs";
 import path from "path";
+import React from "react";
 
 export default function Home({ data }) {
   console.log(data);
-  return <div>index</div>;
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>ジャンル名</th>
+          <th>曲名</th>
+          <th>アーティスト</th>
+          <th>バージョン</th>
+          <th>紹介</th>
+          <th>バナー</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(({ version, music_list, title, index, musicchar }) => (
+          <React.Fragment key={version}>
+            {music_list
+              // .filter((m) => m.detail)
+              .map((m) => (
+                <tr key={m.banner}>
+                  <td>{m.title || ""}</td>
+                  <td>{m.title || ""}</td>
+                  <td>{m.title || ""}</td>
+                  <td>{title}</td>
+                  <td>{m.detail && <a href={m.detail}>楽曲紹介</a>}</td>
+                  <td>
+                    <a href={m.banner}>バナー</a>
+                  </td>
+                </tr>
+              ))}
+          </React.Fragment>
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export async function getStaticProps(context) {
