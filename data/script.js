2ee5b037d966e2cfdf743edb23ac367b54bdd1e5
ssdh233 <songsongdahu233@gmail.com>
2021-12-25 04:02:19 +0900

    Update for 16~

--- data/script.js
+++ data/script.js
@@ -18,26 +18,7 @@
   console.log(JSON.stringify(arr));
 })();
 
-// for 21
-(function () {
-  const arr = Array.from(document.querySelectorAll("img[width='205']")).map(
-    (x) => {
-      if (x.parentNode.nodeName === "A") {
-        return {
-          detail: x.parentNode.href,
-          banner: x.src,
-        };
-      } else {
-        return {
-          banner: x.src,
-        };
-      }
-    }
-  );
-  console.log(JSON.stringify(arr));
-})();
-
-// for 20
+// for 21(205), 20(183), 18(265), 17(260)
 (function () {
   const arr = Array.from(document.querySelectorAll("img[width='183']")).map(
     (x) => {
@@ -54,5 +35,49 @@
     }
   );
   console.log(arr.length);
+  console.log(JSON.stringify(arr));
+})();
+
+// for 19
+(function () {
+  const arr = Array.from(document.querySelectorAll("img[width='257']")).map(
+    (x) => {
+      const banner =
+        "http://web.archive.org/web/20180920035327im_/" +
+        x.src.replace(".gif", "_1.gif");
+      if (x.parentNode.nodeName === "A") {
+        return {
+          detail:
+            "http://web.archive.org/web/20180920035327_id/" + x.parentNode.href,
+          banner,
+        };
+      } else {
+        return {
+          banner,
+        };
+      }
+    }
+  );
+  console.log(arr.length);
+  console.log(JSON.stringify(arr));
+})();
+
+// for pmp 1
+(function () {
+  const arr = Array.from(
+    document.querySelectorAll("#CharactersIndexLayer > a")
+  ).map((x) => ({
+    detail: "https://web.archive.org/web/20160613195301id_/" + x.href,
+  }));
+  console.log(JSON.stringify(arr));
+})();
+
+// for 16
+
+(function () {
+  const arr = Array.from(document.querySelectorAll("map > area")).map((x) => ({
+    detail: x.href,
+    genre: x.title,
+  }));
   console.log(JSON.stringify(arr));
 })();
